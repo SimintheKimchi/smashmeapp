2ca9470351f04199320a9f95ff1c07828cd44aa8
Daniel Sim <anwoo1214@gmail.com>
2019-05-29 09:01:02 -0700

    Finished character roster named all character buttons added example of one character it's up to Saul now

--- app/src/main/java/com/example/smashme/Menu.java
+++ app/src/main/java/com/example/smashme/Menu.java
@@ -1,13 +1,37 @@
 package com.example.smashme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Menu extends AppCompatActivity {
+
+    private ImageButton mario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        wireWidgets();
+
+        mario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageViewActivity();
+            }
+        });
+    }
+
+    private void imageViewActivity() {
+        Intent intentLoadNewActivty = new Intent(Menu.this, Mario.class);
+        startActivity(intentLoadNewActivty);
+    }
+
+    private void wireWidgets(){
+        mario = findViewById(R.id.button_main_mario);
     }
 }
